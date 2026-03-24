2a02c5c875cb63af0f3e604f297f91db6c1ffc8d
LeszczynskiKarol <leszczu123@wp.pl>
2026-03-24 18:02:17 +0100

    zmiana w deployu

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -5,9 +5,9 @@ module.exports = {
       name: "stojan-backend",
       script: "dist/index.js",
       cwd: "/home/ec2-user/stojan-shop-new/backend",
-      interpreter: "/home/ec2-user/.nvm/versions/node/v20.20.0/bin/node",
       env: {
         NODE_ENV: "production",
+        NODE_PATH: "/home/ec2-user/stojan-shop-new/backend/node_modules",
       },
     },
   ],
